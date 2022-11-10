22700cec32e1f5e3335b09a826643162fea2cbaf
arsxlanali <arsxlanali@gmail.com>
2022-11-10 22:38:45 +0500

    removed the admin access only in getSignature route

--- routes/api/cloudinary.js
+++ routes/api/cloudinary.js
@@ -4,9 +4,7 @@ const cloudinaryController = require("../../controllers/cloudinaryController");
 const ROLES_LIST = require("../../config/roles_list");
 const verifyRoles = require("../../middleware/verifyRoles");
 
-router
-  .route("/getSignature")
-  .get(verifyRoles(ROLES_LIST.Admin), cloudinaryController.getSignature);
+router.route("/getSignature").get(cloudinaryController.getSignature);
 
 // router
 //   .route("/uploadImg")
